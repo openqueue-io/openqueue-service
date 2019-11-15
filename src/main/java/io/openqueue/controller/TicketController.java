c4e8c4e956d2e33e903864f77f794d241bd458b6
Jing Chen <jingchen9412@gmail.com>
2019-11-15 23:34:33 +0800

    Update TicketController.java

--- src/main/java/io/openqueue/controller/TicketController.java
+++ src/main/java/io/openqueue/controller/TicketController.java
@@ -4,7 +4,6 @@ import io.openqueue.service.TicketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
